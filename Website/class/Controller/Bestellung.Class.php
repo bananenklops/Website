c48a3b66cec0302ea733bdcc1a95218c3cefdf65
bananenklops <kessler-tobias@gmx.net>
2017-12-12 21:27:46 +0100

    Controller hinzugefügt (muss wahrscheinlich noch überdacht werden)

---
+++ Website/class/Controller/Bestellung.Class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tobias
+ * Date: 12.12.2017
+ * Time: 21:18
+ */
+
+namespace Controller\Bestellung;
+
+
+abstract class Bestellung
+{
+    protected $type = "";
+    protected $list = array();
+}
